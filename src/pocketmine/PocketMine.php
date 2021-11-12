d8f505b00407ea8c7deee11f5e1de256ee8eef73
ZEENPOSTER <87850291+ZEENPOSTER@users.noreply.github.com>
2021-11-12 20:34:30 +0700

    bruh

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -77,7 +77,7 @@ namespace pocketmine {
 	use pocketmine\wizard\Installer;
 
 	const VERSION = "1.1dev";
-	const API_VERSION = "3.0.1";
+	const API_VERSION = "3.0.2";
 	const CODENAME = "LlamaSpit";
 	const GENISYS_API_VERSION = '2.0.0';
 
